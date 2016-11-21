e7a74874c07cc8a02a96ed2acc2a9e1c2e3a588a
draenbed <ivan.kiskin@wadh.ox.ac.uk>
2016-11-21 13:50:25 +0000

    Initial version to use on cluster

---
+++ CNNlib/CNNBirdclef.py
@@ -0,0 +1,125 @@
+#from __future__ import print_function
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from scipy.io.wavfile import read, write
+import os, os.path
+from scipy import signal
+import IPython
+np.random.seed(1337)  # for reproducibility
+
+import random
+
+
+
+###############################################################################################################
+
+def proc_data(path, img_height, img_width, nfft = 512, overlap = 128, normalise = False):
+    """Supply a list of paths in path to return training data with number of classes equal to the length of the path list.
+    Dataset named after string data_name."""
+    data_dct = {}
+    dirs = []
+    
+    for item in path:
+        dirs.append(os.listdir(item))
+    for index, item in enumerate(dirs):
+        signal_list = []
+        list_name = 'class_' + str(index)
+        for file in dirs[index]:
+            if file.endswith('.wav'):
+                fs, signal = read(path[index] + file)
+                if normalise:
+                    print 'Normalising *.wav...'
+                    signal_list.append(signal/np.std(signal))
+                else:
+                    signal_list.append(signal)
+        data_dct.update({list_name: [signal_list]})
+        
+    
+    ###################################################################################################################
+    spec_list_positive = []
+    spec_list_negative= []
+    # Select whole list as training data (min: want equal number of recordings per class)
+    n_train = min(np.shape(data_dct['class_0'])[1], np.shape(data_dct['class_1'])[1]) 
+    #n_train = 5 # Use only n_train recordings as training data (2*n_train for positive and negative)
+    for i in np.arange(n_train):
+        sample = data_dct['class_0'][0][i]
+        sample_negative = data_dct['class_1'][0][i]
+        print 'Processing recording #', i
+        SpecInfo = plt.specgram(sample, Fs = fs, NFFT = nfft, noverlap = overlap)
+        spec_list_positive.append(SpecInfo)
+        SpecInfo = plt.specgram(sample_negative, Fs = fs, NFFT = nfft, noverlap = overlap)
+        spec_list_negative.append(SpecInfo)
+    spec_list = {'class_0': spec_list_positive, 'class_1': spec_list_negative}
+    print 'Number of recordings used as positive data', np.shape(spec_list_positive)
+    print 'Number of recordings used as negative data', np.shape(spec_list_negative)
+    print 'sample rate', fs
+    
+    ###################################################################################################################    
+    # Dimensions for image fed into network
+    nb_classes = 2
+    x_train = []
+    y_train_count = []
+    window_count = []
+    # Stack positive and negative training samples into a single array
+    #spec_training_list = np.vstack([spec_training_list_positive, spec_training_list_negative]) 
+    spec_training_list = spec_list['class_0'] + spec_list['class_1']
+    # Format into same shape as mnist_cnn.py example
+    for i in np.arange(len(spec_training_list)):
+        n_max = np.floor(np.divide(np.shape(spec_training_list[i][0])[1],img_width)).astype('int')
+        if i < len(spec_training_list)/2:
+            window_count.append([n_max, 1])
+        else:
+            window_count.append([n_max, 0])
+        print 'Processing signal number', i
+        print 'Number of training inputs for this signal:', n_max    
+        for n in np.arange(n_max):
+            x_train.append(spec_training_list[i][0][:img_height,img_width*n:img_width*(n+1)])
+            if i < len(spec_training_list)/2:
+                y_train_count.append(np.array(1))
+            else:
+                y_train_count.append(np.array(0))
+
+    x_train = np.array(x_train).reshape((np.shape(x_train)[0],1,img_height,img_width))
+
+    y_train = np.zeros((len(y_train_count),nb_classes))
+
+    y_train[np.where(y_train_count),0] = 1
+    y_train[np.where(np.logical_not(y_train_count)),1] = 1
+    y_positive_frac = np.shape(np.where(y_train_count))[1]/(1.*len(y_train))
+
+    print '\nx dimensions', np.shape(x_train)
+    print 'y dimensions', np.shape(y_train)
+    print 'Fraction of positive samples', y_positive_frac
+
+    input_shape = (1, img_height, img_width)
+    
+    return x_train, y_train, y_positive_frac, input_shape, window_count   
+
+###############################################################################################################
+
+def normalise(data, wrt_data):
+    """Normalise Theano-shaped data arrays for 2D convolutional networks with respect to data in `wrt_data'"""
+    var_data = np.var(wrt_data, axis = 0)
+
+    return data/var_data, var_data
+
+
+################################## Code Execution #############################################################
+
+path_training = ['../../../../Resources/KIBA/BirdCLEF/training/wav/colibri/',
+                 '../../../../Resources/KIBA/BirdCLEF/training/wav/other/']
+
+x_train, y_train, y_positive_frac, input_shape, window_count_train =  proc_data(path_training, 256, 256, normalise = False)   
+
+path_testing = ['../../../../Resources/KIBA/BirdCLEF/testing/wav/colibri/',
+                 '../../../../Resources/KIBA/BirdCLEF/testing/wav/other/']
+x_test, y_test, y_positive_frac_test, input_shape, window_count = proc_data(path_testing, 256, 256, normalise = False)
+
+x_train_norm, x_train_var = normalise(x_train, x_train)
+x_test_norm, x_train_test_var = normalise(x_test, x_train)
+
+###############################################################################################################
+
+np.savez('CNNdata.npz', x_train_norm, x_test, x_test_norm, y_train, y_test, input_shape, window_count)
---
+++ CNNlib/CNNClassifier.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Keras-related imports
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution1D, MaxPooling2D, Convolution2D
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+
+##################################################################
+dataset = np.load('CNNdata.npz')
+
+x_train = dataset["arr_0"]
+x_test_norm = dataset["arr_1"]  # Normalised version of x_test
+x_test = dataset["arr_2"]
+y_train = dataset["arr_3"]
+y_test = dataset["arr_4"]
+input_shape = dataset["arr_5"]
+window_count = dataset["arr_6"]
+
+
+# CNN parameters
+
+# number of convolutional filters
+nb_filters = 3
+
+# size of pooling area for max pooling
+pool_size = (2,2)
+
+# convolution kernel size 
+kernel_size = (5,5)
+
+# number of classes
+nb_classes = np.shape(y_train)[1]
+
+# Initialise model
+
+model = Sequential()
+
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+                       border_mode = 'valid',
+                       input_shape = input_shape))
+model.add(Activation('relu'))
+
+# model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size = pool_size))
+#model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+
+# Train model
+
+batch_size = 16
+nb_epoch = 5
+
+# For practice code run
+# model.fit(X_train_set, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+#          verbose=1, validation_split = 0.1) ## validation_data=(X_test_set, Y_test)
+# score = model.evaluate(X_test_set, Y_test, verbose=0)
+# predictions = model.predict(X_test_set)
+# print('Test score:', score[0])
+# print('Test accuracy:', score[1])
+
+# For spectrogram data run
+x_train_log = 10*np.log10(x_train)
+#x_test_log = np.log(x_test)
+model.fit(x_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=1, validation_split = 0.1) ## validation_data=(X_test_set, Y_test)
+
+
+
+score = model.evaluate(x_test_norm, y_test, verbose=1)
+predictions = model.predict(x_test)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+
+
+positive_predictions = predictions[:,0][np.where(y_test[:,0])]
+negative_predictions = predictions[:,1][np.where(y_test[:,1])]
+
+true_positive_rate = np.floor(sum(positive_predictions))/sum(y_test[:,0])
+true_negative_rate = np.floor(sum(negative_predictions))/sum(y_test[:,1])
+
+negative_sample_start = np.where(y_test[:,1])[0][0]
+
+
+plt.figure(figsize = (15,6))
+#plt.plot(y_test[:,0], 'b', linestyle = '--', linewidth = 2, label = 'y_test_positive')
+plt.plot(predictions[:,0],'g.', label = 'y_pred_positive')
+plt.plot(predictions[:,1], 'y.', label = 'y_pred_negative')
+#[plt.axvline(_x, linewidth=1, color='kx') for _x in ([1,2,3])]
+
+xp = 0
+xn = negative_sample_start
+for i in window_count:
+    if xp < negative_sample_start:
+        xp += i[0]
+    else:
+        xn += i[0]
+    plt.axvline(x=xp, color = 'k', ls = 'dashed', linewidth = 0.5)
+    plt.axvline(x=xn, color = 'r', ls = '--', linewidth = 0.5)
+plt.axvline(x=negative_sample_start, color = 'b', ls = '-', linewidth = 2, label = 'Class boundary')
+
+    
+plt.legend(loc = 7)
+plt.ylim([-0.2,1.2])
+plt.ylabel('Softmax output')
+plt.xlabel('Signal window number')
+plt.show()
+plt.savefig('ClassifierOut.png')
+print 'True positive rate', true_positive_rate, 'True negative rate', true_negative_rate
